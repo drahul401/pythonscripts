03f7b9b38e5d236f68035a0496b5f6134469b20c
Rahul <38392705+drahul401@users.noreply.github.com>
2018-04-21 12:26:42 +0530

    Add files via upload

---
+++ Tryexcept.py
@@ -0,0 +1,17 @@
+##def print_even_odd():
+##    x=int(Input("Enter No : "))
+##    if(x%2==0):
+##        print("Num is even", x)
+##    else:
+##        print("Num is odd", x)
+##print_even_odd()
+def print_even_odd():
+        try:
+           x=int(input("NUM: "))
+           if (x%2==0):
+                   print("even",x)
+           else:
+                   print("odd",x)
+        except:
+                if  __name__ == "__main__":
+                        print_even_odd()                
